f1587ec76745930b3ff7c5a1a901049299cf30ae
wasilina83 <angelinaengelmann@live.de>
2023-10-22 22:24:46 +0200

    some stuff

--- kapaopt.py
+++ kapaopt.py
@@ -1,7 +1,7 @@
 import json as js
 from collections import defaultdict
 from optionsgenerator import dev_options_form_parts
-
+import itertools
 
 class Job :
     def __init__(self, operation, part, quantity, production_time, equip_time):
@@ -94,5 +94,51 @@ jobs_data = get_job_data_form_json("data.json")
 for operation, schedule_list in get_schedule_from_json("data.json").items() :
      job_list = jobs_data[operation]
      maschine_plan = generate_maschine_plan(schedule_list, job_list, get_max_time())
-     
-#dev_options_form_parts()
+
+
+# Die Funktion dev_options_form_parts kann zur Erzeugung aller möglichen Job-Reihenfolgen verwendet werden.
+def generate_job_permutations(jobs_data):
+    jobs = []
+    for operation, job_list in jobs_data.items():
+        for job in job_list:
+            jobs.append(job)
+
+    # Erzeugen Sie alle möglichen Permutationen der Jobs.
+    job_permutations = itertools.permutations(jobs)
+
+    return job_permutations
+
+# Diese Funktion berechnet die Gesamtkosten für einen gegebenen Zeitplan.
+def calculate_total_cost(schedule, max_time):
+    # Hier implementieren Sie die Berechnung der Gesamtkosten für den Zeitplan.
+    # Sie müssen die Maschinenzeitpläne und die Ressourcenkapazität (max_time) berücksichtigen.
+    pass
+
+def brute_force_job_shop_scheduling(jobs_data, schedule_lists, max_time):
+    best_schedule = None
+    best_cost = float('inf')
+
+    job_permutations = generate_job_permutations(jobs_data)
+
+    for permutation in job_permutations:
+        # Erstellen Sie einen Zeitplan basierend auf der aktuellen Permutation.
+        current_schedule = generate_schedule(permutation, schedule_lists)
+
+        # Berechnen Sie die Gesamtkosten für den aktuellen Zeitplan.
+        current_cost = calculate_total_cost(current_schedule, max_time)
+
+        # Aktualisieren Sie den besten Zeitplan, wenn der aktuelle Zeitplan kostengünstiger ist.
+        if current_cost < best_cost:
+            best_cost = current_cost
+            best_schedule = current_schedule
+
+    return best_schedule, best_cost
+
+# Beispielaufruf:
+jobs_data = get_job_data_form_json("data.json")
+schedule_lists = get_schedule_from_json("data.json")
+max_time = get_max_time()
+
+best_schedule, best_cost = brute_force_job_shop_scheduling(jobs_data, schedule_lists, max_time)
+print("Best Schedule:", best_schedule)
+print("Best Cost:", best_cost)
